14c0b2c41c5d1eb41a37eaba088fa638f3b73077
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-08-04 10:51:39 +0200

    deploy fix

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/<recipe-frontend>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<recipe-frontend>/index.html'));});
-app.listen(process.env.PORT || 8080);
